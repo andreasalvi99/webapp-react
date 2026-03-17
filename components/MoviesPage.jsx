1745c73395c88adf8073afd3d5a7f78b686ec8bb
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-17 13:08:13 +0100

    map movies

--- components/MoviesPage.jsx
+++ components/MoviesPage.jsx
@@ -1,3 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function () {
-  return <h1>MoviesPage</h1>;
+  const [movies, setMovies] = useState([]);
+
+  function fetchMovies() {
+    axios.get("http://localhost:3000/movies").then((response) => {
+      console.log(response.data.results);
+      setMovies(response.data.results);
+    });
+  }
+  useEffect(fetchMovies, []);
+
+  return (
+    <div className="row row-cols-5 g-3">
+      {movies.map((movie) => {
+        return (
+          <div className="col">
+            <div class="card h-100">
+              <img src={movie.image} class="card-img-top" alt={movie.title} />
+              <div class="card-body">
+                <h5 class="card-title">{movie.title}</h5>
+                <p class="card-text">{movie.abstract}</p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
