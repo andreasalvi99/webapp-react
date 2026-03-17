db440c8cbe872a11e1083bf5bbe15caf750c381e
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-17 12:08:25 +0100

    install bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+//^ BOOTSTRAP
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
+
 import "./assets/css/index.css";
 import App from "./App.jsx";
 
