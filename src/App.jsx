0dcfd926893f01436fd1486c88585321353e953b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-17 12:16:38 +0100

    set homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route index element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
