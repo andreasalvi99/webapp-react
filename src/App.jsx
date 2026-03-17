ee8931d4697fd275549d8b844f5c7e6a0f9af5af
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-17 12:35:55 +0100

    create moviespage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import DefaultLayout from "../layouts/DefaultLayout";
+import MoviesPage from "../components/MoviesPage";
 
 export default function App() {
   return (
@@ -8,6 +9,8 @@ export default function App() {
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:id" element={<HomePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
