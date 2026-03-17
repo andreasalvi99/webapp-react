e053c174a2ca21ffeeb8f7e79c0d87db4de4b228
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-17 12:12:43 +0100

    set route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
