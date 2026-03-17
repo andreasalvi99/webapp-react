d7cdaf70938d57a6e767fb242419e76081f95edf
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-17 15:19:15 +0100

    add conditional rendering

--- pages/MovieDetailPage.jsx
+++ pages/MovieDetailPage.jsx
@@ -9,39 +9,40 @@ export default function MovieDetailPage() {
 
   function fetchMovieInfo() {
     axios.get(`http://localhost:3000/movies/${id}`).then((response) => {
-      console.log(response.data.results);
       setInfo(response.data.results);
     });
   }
 
   return (
     <>
-      <div className="d-flex gap-3">
-        <div className="card">
-          <img src={info.image} className="card-img-top" alt={info.title} />
-          <div className="card-body">
-            <p className="card-text">{info.abstract}</p>
+      {info && (
+        <div className="d-flex gap-3">
+          <div className="card">
+            <img src={info.image} className="card-img-top" alt={info.title} />
+            <div className="card-body">
+              {/* <p className="card-text">{info.abstract}</p> */}
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Recensioni</h5>
+              <h6 className="card-subtitle mb-2 text-body-secondary">
+                Card subtitle
+              </h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
           </div>
         </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <h6 className="card-subtitle mb-2 text-body-secondary">
-              Card subtitle
-            </h6>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card’s content.
-            </p>
-            <a href="#" className="card-link">
-              Card link
-            </a>
-            <a href="#" className="card-link">
-              Another link
-            </a>
-          </div>
-        </div>
-      </div>
+      )}
     </>
   );
 }
