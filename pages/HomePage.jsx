1a5e05965101032018a49b253d3404057199d095
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-18 22:11:43 +0100

    create suggested film in homepage

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import InfoCard from "../components/InfoCard";
+
 export default function HomePage() {
+  const [suggestedFilm, setSuggestedFilm] = useState();
+  useEffect(() => {
+    async function fetchSuggestedFilm() {
+      const moviesResponse = await axios.get("http://localhost:3000/movies");
+
+      const movies = moviesResponse.data.results;
+      console.log(movies);
+
+      const randomNum = Math.floor(Math.random() * movies.length);
+      setSuggestedFilm(movies[randomNum]);
+    }
+
+    fetchSuggestedFilm();
+  }, []);
+
   return (
     <>
-      <h1>Homepage</h1>
+      {suggestedFilm && (
+        <div className="row row-cols-3">
+          <div className="col">
+            <InfoCard info={suggestedFilm} />
+          </div>
+        </div>
+      )}
     </>
   );
 }
