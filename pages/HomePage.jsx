d34200234c5393a02e37a5f1d8a3d879bff6b66b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-18 22:32:18 +0100

    add link from hompepage to movie info

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import InfoCard from "../components/InfoCard";
+import Rating from "../components/Rating";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   const [suggestedFilm, setSuggestedFilm] = useState();
@@ -23,7 +24,27 @@ export default function HomePage() {
       {suggestedFilm && (
         <div className="row row-cols-3">
           <div className="col">
-            <InfoCard info={suggestedFilm} />
+            <Link to={"/movies/" + suggestedFilm.id}>
+              <div className="card">
+                <img
+                  src={suggestedFilm.image}
+                  className="card-img-top"
+                  alt={suggestedFilm.title}
+                />
+              </div>
+            </Link>
+          </div>
+          <div className="col">
+            <div className="card border-dark mb-3">
+              <div className="card-header">{suggestedFilm.title}</div>
+              <div className="card-body">
+                <h5 className="card-title">{suggestedFilm.title}</h5>
+                <p className="card-text">{suggestedFilm.description}</p>
+                <p className="card-text">
+                  <Rating vote={suggestedFilm.rating} maxVote={5} />
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       )}
