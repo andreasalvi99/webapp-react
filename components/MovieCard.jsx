7d5bb19c491cb12dfa5372d635ee7ccfc69ef5cd
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-18 20:56:25 +0100

    css film cards changes

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function MovieCard({ id, image, title }) {
   return (
     <div className="col">
-      <div className="card" id="card">
+      <div className="card">
         <Link to={"/movies/" + id}>
-          <img src={image} className="card-img-top" alt={title} />
+          <img src={image} className="card-img-top movie-poster" alt={title} />
         </Link>
         <div className="card-body">
           <p className="card-text text-center fw-bold">
