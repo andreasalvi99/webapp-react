f8b34a63204d22087d8bf6ea2919e10453b85cc3
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-19 16:11:00 +0100

    css adjustments

--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -3,7 +3,7 @@ export default function InfoCard({ info }) {
     <div className="card h-100 text-bg-dark">
       <img src={info.image} className="card-img-top" alt={info.title} />
       <div className="card-body">
-        <p className="card-text">{info.abstract}</p>
+        <p className="card-text text-center">{info.abstract}</p>
       </div>
     </div>
   );
